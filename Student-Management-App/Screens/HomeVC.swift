17b9c9f54f654df3428de528d888fe954d34476a
Sadiq Jatu <sadiqjatu@gmail.com>
2026-06-27 00:15:14 +0530

    Remove Storyboard Files

---
+++ Student-Management-App/Screens/HomeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVC.swift
+//  Student-Management-App
+//
+//  Created by Sadiq Jatu on 26/06/26.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
